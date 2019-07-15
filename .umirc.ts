160c84f39d376e9af3fba45faad93f35e46dc706
hfpp2012 <hfpp2012@gmail.com>
2019-07-15 20:39:34 +0800

    chunks

--- .umirc.ts
+++ .umirc.ts
@@ -10,13 +10,13 @@ const config: IConfig =  {
       dva: {
         immer: true,
       },
-      dynamicImport: {
-        loadingComponent: './components/Loading',
-        webpackChunkName: true,
-        level: 2
-      },
+      dynamicImport: false,
+      chunks: ['vendors', 'umi'],
       title: 'myapp',
-      dll: false,
+      dll: {
+        include: ["dva/router", "dva/saga", "dva/fetch"],
+        exclude: ["@babel/runtime"],
+      },
       locale: {
         default: 'zh-CN', //默认语言 zh-CN
         baseNavigator: true, // 为true时，用navigator.language的值作为默认语言
@@ -30,6 +30,23 @@ const config: IConfig =  {
       },
     }],
   ],
+  chainWebpack(config) {
+    config.optimization.splitChunks({
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/](dva|antd|react|react-dom|react-router|react-router-dom)[\\/]/,
+        },
+        commons: {
+          name: 'commons',
+          chunks: 'async',
+          minChunks: 2,
+          minSize: 0,
+        },
+      },
+    });
+  },
 }
 
 export default config;
